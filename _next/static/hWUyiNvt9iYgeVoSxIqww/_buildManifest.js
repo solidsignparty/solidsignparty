580662eb20b5ce5201d14b5ce57c84c244a72f63
rkashapov <hardtechnik91@gmail.com>
2022-09-16 08:36:00 +0000

    Deploying to gh-pages from @ solidsignparty/solidsignparty.github.io@b540d1267ca1da5afcf787e650af1a151c8ee222 🚀

---
+++ _next/static/hWUyiNvt9iYgeVoSxIqww/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/770-b8ed85b291c4617b.js","static/chunks/pages/index-7f7024cc81479c85.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
