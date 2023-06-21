dd6d2e05e77b0d07c62f8caaecc7d0e5bc580097
Rustam Kashapov <hardtechnik91@gmail.com>
2023-06-21 12:43:49 +0300

    solid sign special #2

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Stack from '@mui/material/Stack';
 import PageWithMenu from '../components/PageWithMenu';
 import Party from '../components/Party';
 
+import solidSign5 from '../public/images/solid-sign-5.jpg';
 import solidSign4 from '../public/images/solid-sign-4.jpg';
 import solidSign3 from '../public/images/solid-sign-3.jpg';
 import solidSign2 from '../public/images/solid-sign-2.jpg';
@@ -16,9 +17,13 @@ export default () => {
         spacing={5}
       >
         <Party
+          date={new Date('2023-06-30')}
+          img={solidSign5}
+          href="https://nabchelny.qtickets.events/75912-solid-sign-special-2"
+        />
+        <Party
           date={new Date('2023-05-26')}
           img={solidSign4}
-          href="https://nabchelny.qtickets.events/72807-solid-sign-uu"
         />
         <Party
           date={new Date('2023-04-28')}
