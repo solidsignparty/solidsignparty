83af58617786d8d86d3ea5004bc2d1eea37fccaf
Rustam Kashapov <hardtechnik91@gmail.com>
2023-08-25 15:15:51 +0300

    add harsh, chage ss 4.0 image

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Stack from '@mui/material/Stack';
 import PageWithMenu from '../components/PageWithMenu';
 import Party from '../components/Party';
 
+import harsh from '../public/images/harsh.jpg';
 import urbantatar from '../public/images/urbantatar.jpg';
 import solidSign6 from '../public/images/solid-sign-6.jpg';
 import solidSign5 from '../public/images/solid-sign-5.jpg';
@@ -24,6 +25,10 @@ export default () => {
           href="https://nabchelny.qtickets.events/78444-solid-sign-40"
         >
           <iframe width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1507329757&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>        </Party>
+        <Party
+          date={new Date('2023-08-25')}
+          img={harsh}
+        />
         <Party
           date={new Date('2023-08-19')}
           img={urbantatar}
